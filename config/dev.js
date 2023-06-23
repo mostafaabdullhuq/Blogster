dfbbf59d68f1e2c55336fff5c9bd5a9fe1582f49
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-06-23 21:11:46 +0300

    bug in ci

--- config/dev.js
+++ config/dev.js
@@ -5,9 +5,9 @@ const ENV = dotenv.config().parsed;
 module.exports = {
     googleClientID: "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
     googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-    mongoURI: ENV.MONGODB_URI || process.env.MONGODB_URI,
+    mongoURI: ENV?.MONGODB_URI || process.env.MONGODB_URI,
     cookieKey: "123123123",
     baseURL: "http://localhost:3000/",
-    apiPort: ENV.PORT || 3000,
+    apiPort: ENV?.PORT || 3000,
     redisURL: "redis://127.0.0.1:6379",
 };
